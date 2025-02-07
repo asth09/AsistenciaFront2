b8b2194cce0ff04cc9a1f1c6046d8057ca3bd6e9
s3uz-dev <suzzenelson@gmail.com>
2025-02-07 15:14:27 -0300

    cambio api url

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "./axios";
 
- export const registerRequest = async (user) => axios.post(`/api/register`, user); 
+ export const registerRequest = async (user) => axios.post(`/register`, user); 
 
- export const loginRequest = async (user) => axios.post(`/api/login`, user);
+ export const loginRequest = async (user) => axios.post(`/login`, user);
 
- export const verifyTokenRequest = async () => axios.get(`/api/verify`);
+ export const verifyTokenRequest = async () => axios.get(`/verify`);
